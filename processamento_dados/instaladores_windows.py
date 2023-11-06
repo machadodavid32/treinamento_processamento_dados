a6bfe4fa4725744d1abe0f33a6de507effd7a70d
David Machado <machadodavid32@gmail.com>
2023-11-06 17:53:23 -0300

    Adicionando treinamento sobre como fazer um instalador de programas no windows

--- processamento_dados/instaladores_windows.py
+++ processamento_dados/instaladores_windows.py
@@ -1,5 +1,13 @@
 from time import sleep
 
+""" 
+primeiro passso: Gerar um executável com cxfreeze
+segundo passo: Gerar um instalador
+Para o segundo passo, baixar um software chamado inno setup
+"""
+
 for number in range(30):
     print(number)
     sleep(1)
+    
+    
